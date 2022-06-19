0b82f80b575b7d8af74aee7f65a0a7f750aa478f
nangnangnanG <alim1115@naver.com>
2022-06-20 05:06:23 +0900

    1

---
+++ 이아림.py
@@ -0,0 +1,29 @@
+#num = int(input("정수 : "))
+#if num == 0:
+#    print("0")
+#if num > 0:
+#    print("양수")
+#else:
+#    print("음수")
+
+
+#tem = int(input("기 온도 입력"))
+
+#if tem >= 40 :
+#    print("팬 가동")
+#else:
+#    print ("팬 중지")
+
+  
+#score = int(input('점수ㅠ를 입력하시오'))
+
+#if score > = 90:
+    #print('A')
+#elif score >= 80:
+      #print('B')
+#elif score >= 70:
+      #print ('C')
+#elif score >= 60;
+      #print ('D')
+#else:
+    #print('F')
